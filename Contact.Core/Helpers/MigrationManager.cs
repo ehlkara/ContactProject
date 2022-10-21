b0155e4457e846291e503c027a128c86e75b5494
Ehlullah Karakurt <52125689+ehlkara@users.noreply.github.com>
2022-10-22 00:45:56 +0300

    created migration manager

---
+++ Contact.Core/Helpers/MigrationManager.cs
@@ -0,0 +1,31 @@
+﻿using Abp.UI;
+using Contact.Core.Context;
+using Contact.Models.Errors;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Contact.Core.Helpers
+{
+    public static class MigrationManager
+    {
+        public static IHost MigrateDatabase(this IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                using (var contactContext = scope.ServiceProvider.GetRequiredService<ContactDbContext>())
+                {
+                    try
+                    {
+                        contactContext.Database.Migrate();
+                    }
+                    catch (Exception ex)
+                    {
+                        throw new UserFriendlyException((int)ErrorCodes.NotWorkMigrate, ErrorMessages.NotWorkedMigrate, ex.Message);
+                    }
+                }
+            }
+            return host;
+        }
+    }
+}
