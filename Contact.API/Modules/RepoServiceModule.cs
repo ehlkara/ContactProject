b43e3bb7d4bd1343c47142f1c446bed6f83fb4bc
Ehlullah Karakurt <52125689+ehlkara@users.noreply.github.com>
2022-10-23 16:13:11 +0300

    added dependency injection life cycle and serilog

---
+++ Contact.API/Modules/RepoServiceModule.cs
@@ -0,0 +1,20 @@
+﻿using Autofac;
+using Contact.BusinessLogic.Abstract;
+using Contact.BusinessLogic.ContactServices;
+using Contact.DataAccess.Abstract;
+using Contact.DataAccess.Concrete;
+using Module = Autofac.Module;
+namespace Contact.API.Modules
+{
+    public class RepoServiceModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            //BLL
+            builder.RegisterType<ContactBLL>().As<IContactBLL>().InstancePerLifetimeScope();
+
+            //DAL
+            builder.RegisterType<ContactDAL>().As<IContactDAL>().InstancePerLifetimeScope();
+        }
+    }
+}
--- Contact.API/Program.cs
+++ Contact.API/Program.cs
@@ -1,15 +1,28 @@
+using Autofac.Extensions.DependencyInjection;
+using Autofac;
+using Contact.BusinessLogic.Mapping;
 using Contact.Core.Context;
 using Microsoft.EntityFrameworkCore;
 using System.Reflection;
+using Serilog;
+using Contact.API.Modules;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .Enrich.FromLogContext()
+    .CreateLogger();
+builder.Logging.ClearProviders();
+builder.Logging.AddSerilog(logger);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAutoMapper(typeof(MapProfile));
 
 builder.Services.AddDbContext<ContactDbContext>(x =>
 {
@@ -19,7 +32,34 @@ builder.Services.AddDbContext<ContactDbContext>(x =>
     }).UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
 });
 
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+builder.Host.ConfigureContainer<ContainerBuilder>(containerBuilder => containerBuilder.RegisterModule(new RepoServiceModule()));
+
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    try
+    {
+        var db = scope.ServiceProvider.GetRequiredService<ContactDbContext>();
+        db.Database.Migrate();
+    }
+    catch (Exception ex)
+    {
+        if (Log.Logger == null || Log.Logger.GetType().Name == "SlientLogger")
+        {
+            Log.Logger = new LoggerConfiguration().MinimumLevel.Debug()
+                .WriteTo.Console()
+                .WriteTo.RollingFile("log-{Date}.txt", retainedFileCountLimit: null, outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level}] {Message}{NewLine}{Exception}")
+                .CreateLogger();
+        }
+        Log.Fatal(ex, "Host terminated unexpectedly");
+    }
+    finally
+    {
+        Log.CloseAndFlush();
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
